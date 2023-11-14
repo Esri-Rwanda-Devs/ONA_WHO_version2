108395536caf861019af658b696f29c5636638d7
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-11-14 15:42:22 +0200

    tables fix

--- models/monitoring.js
+++ models/monitoring.js
@@ -1,42 +1,32 @@
 module.exports = (sequelize, DataTypes) => {
-  const Monitoring = sequelize.define(
-    "monitoring",
-    {
-      objectid: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-      },
-      date: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      stationid: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      stationname: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      rain: {
-        type: DataTypes.DECIMAL(38, 8),
-        allowNull: false,
-      },
-      latitude: {
-        type: DataTypes.DECIMAL(38, 8),
-        allowNull: false,
-      },
-      longitude: {
-        type: DataTypes.DECIMAL(38, 8),
-        allowNull: false,
-      },
+  const Monitoring = sequelize.define("monitoring", {
+    objectid: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
     },
-    {
-      timestamps: true,
-    }
-  );
+    date: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    stationid: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    stationname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rain: {
+      type: DataTypes.DECIMAL(38, 8),
+      allowNull: false,
+    },
+    shape: {
+      type: DataTypes.GEOMETRY("POINT"),
+      allowNull: false,
+    },
+  });
 
   return Monitoring;
 };
